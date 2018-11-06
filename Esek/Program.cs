aa46d041f9d8c9dd090f032eaef78dffac8c39a8
SutoSandor <sandorsuto97@gmail.com>
2018-11-06 11:22:41 +0100

    Esek feladat

--- Esek/Program.cs
+++ Esek/Program.cs
@@ -8,8 +8,24 @@ namespace Esek
 {
     class Program
     {
+        private static int megszamol(string[] tomb)
+        {
+            int x = 0;
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i][0] == 'E')
+                {
+                    x++;
+                }
+            }
+            return x;
+        }
+        private static string[] nevek = new string[] { "Erzsébet", "Jóska", "Antal", "Gábor", "Zoltán", "Katalin", "Mária", "Eszter" };
         static void Main(string[] args)
         {
+            Console.WriteLine("E-vel kezdődő nevek száma: " +megszamol(nevek));
+            Console.ReadKey();
         }
+        
     }
 }
